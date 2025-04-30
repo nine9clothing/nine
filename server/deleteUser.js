4b630ac2c4eceecdcae3bafbc56b320969751343
BuddyVerse <shahhenil0212@gmail.com>
2025-05-01 04:19:00 +0530

    your message about the changes

--- server/deleteUser.js
+++ server/deleteUser.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  'https://yehlkbbjlhacdivjnmrf.supabase.co', // replace with your real project URL
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllaGxrYmJqbGhhY2RpdmpubXJmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MzcwMDQ4NCwiZXhwIjoyMDU5Mjc2NDg0fQ.i60_S-W3ykQ6LzwCLEIuMPPMBr_PO_8CoU6AQveycOo' // replace with your real service role key (keep it secret!)
+  '', // replace with your real project URL
+  '' // replace with your real service role key (keep it secret!)
 )
 
 const deleteUser = async (userId) => {
