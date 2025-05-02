4cf984d2217a33ebd11191c598540f15112ad4b2
BuddyVerse <shahhenil0212@gmail.com>
2025-05-03 04:43:25 +0530

    Add Razorpay

--- client/src/pages/Rewards.jsx
+++ client/src/pages/Rewards.jsx
@@ -193,9 +193,9 @@ const Rewards = () => {
             }}>
                 <Navbar showLogo={true} />
                 <div style={{
-                    // flex: '1 0 auto',
-                    // padding: '40px 15px',
-                    // textAlign: 'center'
+                    flex: '1 0 auto',
+                    padding: '0px',
+                    textAlign: 'center'
                 }}>
                     <p style={{
                         fontSize: '1rem',
