1b56a8c1a9cbbbf339a031f7d5efef7cdca39c09
BuddyVerse <shahhenil0212@gmail.com>
2025-05-04 05:48:14 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,40 +4,41 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import AuthProvider from './context/AuthContext';
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Cart from './pages/Cart';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import Sort from './pages/Sort';
-import ProductDetail from './pages/ProductDetail';
-import Success from './pages/Success';
-import Checkout from './pages/Checkout';
-import MyOrders from './pages/MyOrders';
-import WishlistPage from './pages/WishlistPage';
-import ResetPassword from './pages/ResetPassword';
-import FAQ from './pages/policy/FaqPage';
-import ShippingPolicy from './pages/policy/ShippingPolicy';
-import TermsAndConditions from './pages/policy/TermsAndConditions'; 
-import Exchange from './pages/policy/Exchange';
-import MyAccount from './pages/MyAccount';
-import Rewards from './pages/Rewards';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Cart from './pages/Cart';
+// import Contact from './pages/Contact';
+// import Login from './pages/Login';
+// import Sort from './pages/Sort';
+// import ProductDetail from './pages/ProductDetail';
+// import Success from './pages/Success';
+// import Checkout from './pages/Checkout';
+// import MyOrders from './pages/MyOrders';
+// import WishlistPage from './pages/WishlistPage';
+// import ResetPassword from './pages/ResetPassword';
+// import FAQ from './pages/policy/FaqPage';
+// import ShippingPolicy from './pages/policy/ShippingPolicy';
+// import TermsAndConditions from './pages/policy/TermsAndConditions'; 
+// import Exchange from './pages/policy/Exchange';
+// import MyAccount from './pages/MyAccount';
+// import Rewards from './pages/Rewards';
+import ComingSoon from './pages/ComingSoon';
 
-import AdminLayout from './components/AdminLayout';
-import AdminDashboard from './pages/admin/AdminDashboard';
-import AdminAddProduct from './pages/admin/AdminAddProduct'
-import AdminViewProduct from './pages/admin/AdminViewProduct'
-import AdminOrders from './pages/admin/AdminOrders';
-import AdminExchangeOrders from './pages/admin/AdminExchangeOrders';
-import AdminInsights from './pages/admin/AdminInsights';
-import AdminMessages from './pages/admin/AdminMessages';
-import AdminVideo from './pages/admin/AdminAddVideo';
-import AdminViewVideo from './pages/admin/AdminViewVideo'
-import AdminNotifySize from './pages/admin/AdminNotifySize'
-import AdminNewsletter from './pages/admin/AdminSubscription'
-import AdminCartLeftOver from './pages/admin/AdminCartLeftOver'
-import AdminPromoCode from './pages/admin/AdminPromoCode'
-import AdminViewPromoCode from './pages/admin/AdminViewPromoCode'
+// import AdminLayout from './components/AdminLayout';
+// import AdminDashboard from './pages/admin/AdminDashboard';
+// import AdminAddProduct from './pages/admin/AdminAddProduct'
+// import AdminViewProduct from './pages/admin/AdminViewProduct'
+// import AdminOrders from './pages/admin/AdminOrders';
+// import AdminExchangeOrders from './pages/admin/AdminExchangeOrders';
+// import AdminInsights from './pages/admin/AdminInsights';
+// import AdminMessages from './pages/admin/AdminMessages';
+// import AdminVideo from './pages/admin/AdminAddVideo';
+// import AdminViewVideo from './pages/admin/AdminViewVideo'
+// import AdminNotifySize from './pages/admin/AdminNotifySize'
+// import AdminNewsletter from './pages/admin/AdminSubscription'
+// import AdminCartLeftOver from './pages/admin/AdminCartLeftOver'
+// import AdminPromoCode from './pages/admin/AdminPromoCode'
+// import AdminViewPromoCode from './pages/admin/AdminViewPromoCode'
 
 
 
@@ -49,6 +50,8 @@ const App = () => {
         <Routes>
 
           {/* Public Routes */}
+          <Route path="/" element={<ComingSoon />} />
+{/* 
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/checkout" element={<Checkout />} />
@@ -66,10 +69,11 @@ const App = () => {
           <Route path="/t&c" element={<TermsAndConditions />} />
           <Route path="/exchange" element={<Exchange />} />
           <Route path="/my-account" element={<MyAccount />} />
-          <Route path="/rewards" element={<Rewards />} />
+          <Route path="/rewards" element={<Rewards />} /> */}
 
           
-          {/* Admin Routes (with custom layout) */}
+            {/* Admin Routes (with custom layout) */}
+{/*             
           <Route path="/admin" element={<AdminLayout />}>
             <Route path="dashboard" element={<AdminDashboard />} />
             <Route path="products" element={<AdminAddProduct />} />
@@ -86,7 +90,7 @@ const App = () => {
             <Route path="promocode" element={<AdminPromoCode/>} />
             <Route path="viewpromocode" element={<AdminViewPromoCode/>} />
 
-          </Route>
+          </Route> */}
         </Routes>
       </CartProvider>
       </BrowserRouter>
