c3dbee2deedcb10bbd33434170e374749e68d6b1
BuddyVerse <shahhenil0212@gmail.com>
2025-05-30 14:33:18 +0530

    Update

--- client/src/lib/supabase.js
+++ client/src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://yehlkbbjlhacdivjnmrf.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllaGxrYmJqbGhhY2RpdmpubXJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3MDA0ODQsImV4cCI6MjA1OTI3NjQ4NH0.CIn3hl_4CaV405-k64ugE5-oWbwoSC6ZukckBB5zga8'
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
@@ -9,5 +9,4 @@ export const supabase = createClient(supabaseUrl, supabaseKey, {
       persistSession: true,       
     }
 });
-
-export default supabase;
+export default supabase; 
