68c21a12df0fd527ca5974a13c188d96e4642b35
BuddyVerse <shahhenil0212@gmail.com>
2025-05-30 14:23:40 +0530

    Update

--- client/src/lib/supabase.js
+++ client/src/lib/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://yehlkbbjlhacdivjnmrf.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllaGxrYmJqbGhhY2RpdmpubXJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3MDA0ODQsImV4cCI6MjA1OTI3NjQ4NH0.CIn3hl_4CaV405-k64ugE5-oWbwoSC6ZukckBB5zga8'
-
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
       autoRefreshToken: true,      
